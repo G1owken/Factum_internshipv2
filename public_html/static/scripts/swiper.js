ac637d2e3c0ed7ef7cbf6b3a0fec3c8e94792e86
G1owken <bisenislam007@gmail.com>
2026-07-24 14:25:11 +0500

    first try

---
+++ public_html/static/scripts/swiper.js
@@ -0,0 +1,22 @@
+const swiper1 = new Swiper('.home-hero__slider', {
+    loop: false,
+    speed: 800,
+    allowTouchMove: false,
+    direction: 'vertical',
+    spaceBetween: 20
+});
+
+function nextSlide() {
+    setTimeout(() => {
+
+        if (swiper1.isEnd) {
+            swiper1.slideTo(0, 500);
+        } else {
+            swiper1.slideNext();
+        }
+
+        swiper1.once('transitionEnd', nextSlide);
+
+    }, 3000);
+}
+nextSlide();
